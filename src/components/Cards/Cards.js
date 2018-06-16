1adb2407edc5f7d418bcf68101147b394508a2dc
Daniel <olivas1406@msn.com>
2018-06-15 21:59:12 -0700

    Updated Cards.js

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,26 +1,11 @@
 import React from "react";
 import "./Cards.css";
 
-// const Cards = props => (
-//   <div className="card">
-//     <div className="img-container">
-//     <span onClick={() => props.guessFriend(props.id)} className="guess">
-//       <img alt={props.name} src={props.image} />
-//       </span>
-//     </div>
-//   </div>
-// );
-
-
 const Cards = props => (
-  <div className="card">
-      <div className="img-container">
-          <a onClick={() => props.pickedFriends(props.id)} 
-              className={props.score === 0}
-          >
-              <img alt={props.name} src={props.image} />
-          </a>
-      </div>
+	<div onClick={() => props.guessedDaedra(props.id)} className="card">
+		<div className="img-container">
+      		<img alt={props.name} src={props.image} />
+    	</div>
   </div>
 );
 
